b0158d8986394142a597224d19bdf4a75a96671e
Mattl02 <hoemac16@htl-kaindorf.ac.at>
2018-10-04 18:11:57 +0200

    added sortbydate comparator

---
+++ Exercise_103_Terminkalender/src/SortByDate.java
@@ -0,0 +1,21 @@
+
+import java.util.Comparator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Matthias
+ */
+public class SortByDate implements Comparator<Appointment>{
+
+    @Override
+    public int compare(Appointment a, Appointment a1) {
+        return a.getDate().compareTo(a1.getDate());
+    }
+    
+}
